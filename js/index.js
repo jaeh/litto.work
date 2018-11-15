f816e2df399d6e62c5e51a1ab46fadd61f5d6448
Jascha <jascha@jaeh.at>
2018-11-15 22:31:05 +0100

    update public

--- js/index.js
+++ js/index.js
@@ -1,7 +1,20 @@
 var DROP = 'drop'
 
 var dragged = false
+var startPos = false
 var currentZIndex = 1
+var images = document.getElementsByTagName('img')
+var draggables = document.getElementsByClassName('drag')
+var maxZIndex = draggables.length * 5
+
+function hasCl(e, cl) { 
+  return e.className.indexOf(cl) > -1
+}
+
+function doNothing(e) {
+  e.preventDefault()
+  return false
+}
 
 function $(str) {
   return document.getElementById(str)
@@ -12,22 +25,41 @@ function getPos(e) {
 }
 
 function drag(ev) {
-  dragged = ev.target,
-  console.log('drag', dragged)
+  console.log('drag')
+  dragged = ev.target
+  startPos = {
+    left: getPos(dragged.style.left),
+    top: getPos(dragged.style.top),
+  }
+  currentZIndex += 1
+  dragged.style.zIndex = currentZIndex
   dragged.offset = {
     left: ev.clientX - getPos(dragged.style.left),
     top: ev.clientY - getPos(dragged.style.top),
   }
-  currentZIndex += 1
-  dragged.style.zIndex = currentZIndex
+  dragged.style.opacity = 0.8
 
   document.onmousemove = mousemove
   document.onmouseup = drop
 }
 
 function drop(ev) {
-  console.log('drop')
+  if (startPos) {
+    var endPos = {
+      left: getPos(ev.target.style.left),
+      top: getPos(ev.target.style.top),
+    }
+    
+    if (startPos.left === endPos.left && startPos.top === endPos.top) {
+      console.log('click')
+    } else {
+      console.log('drop')
+    }
+  }
+  dragged.style.opacity = 1
+
   dragged = false
+  startPos = false
 
   document.onmousemove = null
   document.onmouseup = null
@@ -59,18 +91,11 @@ function mousemove(ev) {
   }
 }
 
-var images = document.getElementsByTagName('img')
-
-function hasCl(e, cl) { 
-  return e.className.indexOf(cl) > -1
-}
-
 for(var i = 0; i < images.length; i++) {
   if (images.hasOwnProperty(i)) {
     var img = images[i]
-    img.addEventListener('dragstart', function (evt) {
-      evt.preventDefault()
-    })
+    img.addEventListener('dragstart', doNothing)
+    
     if (hasCl(img, 'drag')) {
       img.addEventListener('mousedown', drag)
     }
